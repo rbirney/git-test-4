d1cacd67ed10435d8b5948e1f06e6e448b1ef49f
Glitch (topic-04-lab) <none>
2024-01-11 09:57:44 +0000

    💎🔽 Updated with Glitch

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -1,19 +1,34 @@
-'use strict';
+"use strict";
 
-import logger from '../utils/logger.js';
-import playlistStore from '../models/playlist-store.js';
+import logger from "../utils/logger.js";
+import playlistStore from "../models/playlist-store.js";
+import { v4 as uuidv4 } from "uuid";
 
 const playlist = {
   createView(request, response) {
     const playlistId = request.params.id;
-    logger.debug('Playlist id = ' + playlistId);
-    
+    logger.debug("Playlist id = " + playlistId);
+
     const viewData = {
-      title: 'Playlist',
-      singlePlaylist: playlistStore.getPlaylist(playlistId)
+      title: "Playlist",
+      singlePlaylist: playlistStore.getPlaylist(playlistId),
     };
 
-    response.render('playlist', viewData);
+    response.render("playlist", viewData);
+  },
+
+  addSong(request, response) {
+    const playlistId = request.params.id;
+    const playlist = playlistStore.getPlaylist(playlistId);
+    const newSong = {
+      id: uuidv4(),
+      title: request.body.title,
+      artist: request.body.artist,
+      genre: request.body.genre,
+      duration: request.body.duration,
+    };
+    playlistStore.addSong(playlistId, newSong);
+    response.redirect("/playlist/" + playlistId);
   },
 };
 
--- server.js
+++ server.js
@@ -1,16 +1,46 @@
-'use strict';
+"use strict";
 
-import express from 'express';
+import express from "express";
 import logger from "./utils/logger.js";
-import routes from './routes.js'; 
-import { create } from 'express-handlebars';
+import routes from "./routes.js";
+import { create } from "express-handlebars";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const handlebars = create({extname: '.hbs'});
+const handlebars = create({
+  extname: ".hbs",
+  helpers: {
+    uppercase: (inputString) => {
+      return inputString.toUpperCase();
+    },
+    formatDate: (date) => {
+      let dateCreated = new Date(date);
+      let options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+      };
+      return `${dateCreated.toLocaleDateString("en-IE", options)}`;
+    },
+    capitalise: (str) => {
+      let words = str.split(" ");
+      let formattedname = "";
+      words.forEach(
+        (word) =>
+          (formattedname += `${word.charAt(0).toUpperCase()}${word
+            .substring(1)
+            .toLowerCase()} `)
+      );
+      return formattedname;
+    },
+  },
+});
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
